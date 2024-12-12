836b7f10bee0435b29e2382b5763c54455abba81
F0Rt04ka <rmn.2007@mail.ru>
2024-12-12 22:54:49 +0700

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -6,7 +6,49 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func runeIsNumber(r rune) bool {
+	// руны - символы 48 - 0, 57 - 9
+	return r >= 48 && r <= 57
+}
+
+func Unpack(str string) (string, error) {
+	isPrevEscaped := false
+	var prev rune
+	var result string
+
+	for _, i := range str {
+		if prev == 0 {
+			if runeIsNumber(i) {
+				return "", ErrInvalidString
+			}
+			result += string(i)
+			prev = i
+			continue
+		}
+
+		if runeIsNumber(i) && runeIsNumber(prev) && !isPrevEscaped {
+			return "", ErrInvalidString
+		}
+
+		switch {
+		case runeIsNumber(i) && (prev != '\\' || prev == '\\' && isPrevEscaped):
+			if i == rune('0') {
+				asRune := []rune(result)
+				result = string(asRune[0 : len(asRune)-1])
+			}
+
+			for j := 1; j < int(i)-'0'; j++ {
+				result += string(prev)
+			}
+		case i != '\\':
+			result += string(i)
+		case i == '\\' && prev == '\\' && !isPrevEscaped:
+			result += string(i)
+		}
+
+		isPrevEscaped = prev == '\\' && !isPrevEscaped
+		prev = i
+	}
+
+	return result, nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"testing"
 
+	//nolint:depguard
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,11 +17,12 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: "😀3", expected: "😀😀😀"},
+		{input: "😀3😀0", expected: "😀😀😀"},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
