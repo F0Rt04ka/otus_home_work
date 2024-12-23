03a8065b759c6236c03a6ca7adc0f0351a8682c2
F0Rt04ka <rmn.2007@mail.ru>
2024-12-23 18:20:27 +0700

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,54 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"slices"
+	"strings"
+)
+
+func Top10(str string) []string {
+	if str == "" {
+		return []string{}
+	}
+
+	words := make(map[string]int)
+	max := 0
+
+	for _, word := range strings.Fields(str) {
+		words[word]++
+		if words[word] > max {
+			max = words[word]
+		}
+	}
+
+	outputLen := 10
+	if len(words) < outputLen {
+		outputLen = len(words)
+	}
+	output := make([]string, outputLen)
+
+	for i := 0; i < outputLen; i++ {
+		for word := range words {
+			if slices.Contains(output, word) {
+				continue
+			}
+
+			if output[i] == "" {
+				output[i] = word
+
+				continue
+			}
+
+			if words[word] < words[output[i]] {
+				continue
+			}
+
+			if words[word] == words[output[i]] && strings.Compare(output[i], word) <= 0 {
+				continue
+			}
+
+			output[i] = word
+		}
+	}
+
+	return output
 }
