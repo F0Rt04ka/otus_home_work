5e5407299c88893bb7e221c28a2b3d8ebbe4f94d
F0Rt04ka <rmn.2007@mail.ru>
2025-02-09 21:03:31 +0700

    HW 7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +15,88 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fileFrom, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+
+	fileCloseFn := func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	defer fileCloseFn(fileFrom)
+
+	fileTo, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer fileCloseFn(fileTo)
+
+	fileStat, err := fileFrom.Stat()
+	if err != nil {
+		return err
+	}
+
+	fileSize := fileStat.Size()
+
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if fileStat.Mode()&os.ModeDevice != 0 {
+		return ErrUnsupportedFile
+	}
+
+	if limit > fileSize || limit == 0 {
+		limit = fileSize
+	}
+
+	copyFn := func(from, to *os.File) error {
+		bufferSize := int64(100)
+		if bufferSize > limit {
+			bufferSize = limit
+		}
+
+		buffer := make([]byte, bufferSize)
+		alreadyReadBytes := int64(0)
+
+		bar := pb.StartNew(0)
+		bar.SetTotal(limit)
+		if limit+offset > fileSize {
+			bar.SetTotal(fileSize - offset)
+		}
+
+		defer func() {
+			bar.Finish()
+		}()
+
+		for alreadyReadBytes < limit {
+			readBytes, err := from.ReadAt(buffer, offset+alreadyReadBytes)
+			bar.Add(readBytes)
+
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					alreadyReadBytes = limit
+				} else {
+					return err
+				}
+			}
+
+			alreadyReadBytes += int64(readBytes)
+
+			_, err = to.Write(buffer[:readBytes])
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	copyFn(fileFrom, fileTo)
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,80 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var testFiles = map[string]string{
+	"full":      "./testdata/input.txt",
+	"0_0":       "./testdata/out_offset0_limit0.txt",
+	"0_10":      "./testdata/out_offset0_limit10.txt",
+	"0_1000":    "./testdata/out_offset0_limit1000.txt",
+	"100_1000":  "./testdata/out_offset100_limit1000.txt",
+	"6000_1000": "./testdata/out_offset6000_limit1000.txt",
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("Unsupported file", func(t *testing.T) {
+		err := Copy("/dev/urandom", "/tmp/out.txt", 0, 0)
+		require.EqualError(t, err, ErrUnsupportedFile.Error())
+	})
+
+	t.Run("Offset exceeds file size", func(t *testing.T) {
+		err := Copy(testFiles["full"], "/tmp/out.txt", 10000, 0)
+		require.EqualError(t, err, ErrOffsetExceedsFileSize.Error())
+	})
+
+	t.Run("offset:0 limit:0", func(t *testing.T) {
+		runTest(t, 0, 0)
+	})
+
+	t.Run("offset:0 limit:10", func(t *testing.T) {
+		runTest(t, 0, 10)
+	})
+
+	t.Run("offset:0 limit:1000", func(t *testing.T) {
+		runTest(t, 0, 1000)
+	})
+
+	t.Run("offset:100 limit:1000", func(t *testing.T) {
+		runTest(t, 100, 1000)
+	})
+
+	t.Run("offset:6000 limit:1000", func(t *testing.T) {
+		runTest(t, 6000, 1000)
+	})
+}
+
+func runTest(t *testing.T, offset, limit int64) {
+	t.Helper()
+	file, err := os.CreateTemp("/tmp", "out.txt")
+	require.NoError(t, err)
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	srcFileKey := fmt.Sprintf("%d_%d", offset, limit)
+
+	err = Copy(testFiles["full"], file.Name(), offset, limit)
+	require.NoError(t, err)
+
+	srcInfo, err := os.Stat(testFiles[srcFileKey])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	destInfo, err := os.Stat(file.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, srcInfo.Size(), destInfo.Size())
 }
